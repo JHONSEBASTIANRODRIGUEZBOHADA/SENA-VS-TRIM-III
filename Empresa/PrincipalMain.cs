8b40c4fe4e1c9087c0d0deb8791bf2c3b7e428ab
JHONSEBASTIANRODRIGUEZBOHADA <104032255+JHONSEBASTIANRODRIGUEZBOHADA@users.noreply.github.com>
2022-07-26 16:20:07 -0500

    Add files via upload

---
+++ Empresa/PrincipalMain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empresa
+{
+    internal class PrincipalMain
+    {
+        public static void Main(string[] args)
+        {
+            Procedimientos array1 = new Procedimientos();
+
+            array1.NotaFinal();
+        }
+    }
+}
