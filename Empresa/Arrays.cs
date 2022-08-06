251533bd0e4117bdf2e3e8ec7348f5bedd79abc2
JHONSEBASTIANRODRIGUEZBOHADA <104032255+JHONSEBASTIANRODRIGUEZBOHADA@users.noreply.github.com>
2022-08-06 10:39:53 -0500

    Add files via upload

---
+++ Empresa/Arrays.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empresa
+{
+    internal class Arrays
+    {
+        bool Error;
+        public void FinalGrade()
+        {
+
+            string[] name = { "Felipe", "Oscar", "Andrea", "kimberly" };
+            int[] partialTest = { 0, 6, 4, 9 };
+            int[] oralTest =    { 7, 1, 3, 4 };
+            int[] finalTest =   { 8, 9, 1, 9 };
+            float sum = 0;
+            float average = 0;
+
+            for(int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"\nNAME: {name[i]} \nPARTIAL TEST: {partialTest[i]} \nORAL TEST: {oralTest[i]} \nFINAL TEST: {finalTest[i]}");
+            }
+            
+            for(int i = 0; i < name.Length; i++)
+            {
+                sum = partialTest[i] + oralTest[i] + finalTest[i];
+                average = sum / 3;
+                if (average >= 7)
+                {
+                    Console.WriteLine($"\nNAME: {name[i]} \nAVERAGE RATING: {average} APPROVED");
+                }
+                else
+                {
+                    Console.WriteLine($"\nNAME: {name[i]} \nAVERAGE RATING: {average} DID NOT APPROVE");
+                }
+            }
+        }
+
+        public void CustomerData()
+        {
+            string[] firstName = new string[5];
+            string[] firstSurname = new string[5];
+            int[] age = new int[5];
+            string[] gender = new string[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                Console.WriteLine($"\n{i}. Enter your name");
+                firstName[i] = Console.ReadLine();
+                Console.WriteLine("Enter your last name");
+                firstSurname[i] = Console.ReadLine();
+
+                Error = false;
+                while (!Error)
+                {
+                    try
+                    {
+                        Console.WriteLine("Enter your age");
+                        age[i] = int.Parse(Console.ReadLine());
+                        if (!Error)
+                        {
+                            Error = true;
+                            if (age[i] < 0)
+                            {
+                                Console.WriteLine("Income a negative value");
+                                Error = false;
+                            }
+                        }
+                    }
+                    catch (OverflowException e)
+                    {
+                        Console.WriteLine("You enter a very long age");
+                        Error = false;
+                    }
+                    catch (FormatException e)
+                    {
+                        Console.WriteLine("You Did not enter a numeric value");
+                        Error = false;
+                    }
+                }
+                Console.WriteLine("Enter your gender");
+                gender[i] = Console.ReadLine();
+            }
+        }
+
+        public void SumPriceProducts()
+        {
+            string[] products = new string[5];
+            double[] price = new double[5];
+
+            Console.WriteLine("Enter 5 products with their price");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"\n{i}. Enter product name");
+                products[i] = Console.ReadLine();
+
+                Error = false;
+                while (!Error)
+                {
+                    try
+                    {
+                        Console.WriteLine("Enter the price of the product");
+                        price[i] = double.Parse(Console.ReadLine());
+                        if (!Error)
+                        {
+                            Error = true;
+                            if (price[i] < 0)
+                            {
+                                Console.WriteLine("Income a negative value");
+                                Error = false;
+                            }
+                        }
+                    }
+                    catch (OverflowException e)
+                    {
+                        Console.WriteLine("You enter a very long value");
+                        Error = false;
+                    }
+                    catch (FormatException e)
+                    {
+                        Console.WriteLine("Did not enter a numeric value");
+                        Error = false;
+                    }
+                }
+            }
+            Console.WriteLine($"\nThe total price of your purchase is: {price.Sum()}");
+        }
+    }
+}
---
+++ Empresa/Methods.cs
@@ -0,0 +1,399 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empresa
+{
+    internal class Methods
+    {
+        bool Error;
+        //U.S DOLLAR TO COLOMBIAN PESOS
+        public void CurrencyConverter1()
+        {
+            const double cop = 4418;
+            double usd;
+            double conversion;
+
+            Error = false;
+            while (!Error)
+            {
+                try
+                {
+                    Console.WriteLine("Enter the money (U.S dollar) to be converted into colombian pesos");
+                    usd = double.Parse(Console.ReadLine());
+
+                    conversion = usd * cop;
+
+                    if(!Error)
+                    {
+                        Error = true;
+                        if(usd < 0)
+                        {
+                            Console.WriteLine("Income a negative value");
+                            Error = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"United States dollar: {usd} ---> Colombian pesos {conversion}");
+                            Error = true;
+                        }
+                    }
+                }
+                catch (OverflowException e)
+                {
+                    Console.WriteLine("You enter a very long money");
+                    Error = false;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Did not enter a numeric value");
+                    Error = false;
+                }
+            }
+        }
+
+        //NEW ZEALAND DOLLAR TO COLOMBIAN PESOS
+        public void CurrencyConnverter2()
+        {
+            const double cop = 2736;
+            double nzd;
+            double conversion;
+
+            Error = false;
+            while (!Error)
+            {
+                try
+                {
+                    Console.WriteLine("Enter the money (New Zealand dollar) to be converted into colombian pesos");
+                    nzd = double.Parse(Console.ReadLine());
+
+                    conversion = nzd * cop;
+
+                    if(!Error)
+                    {
+                        Error = true;
+                        if(nzd < 0)
+                        {
+                            Console.WriteLine("Income a negative value");
+                            Error = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"New Zealand dollar: {nzd} ---> Colombian pesos {conversion}");
+                            Error = true;
+                        }
+                    }
+                }
+                catch (OverflowException e)
+                {
+                    Console.WriteLine("You enter a very long money");
+                    Error = false;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Did not enter a numeric value");
+                    Error = false;
+                }
+            }
+        }
+
+        //SINGAPORE DOLLAR TO COLOMBIAN PESOS
+        public void CurrencyConnverter3()
+        {
+            const double cop = 3176;
+            double sgd;
+            double conversion;
+
+            Error = false;
+            while (!Error)
+            {
+                try
+                {
+                    Console.WriteLine("Enter the money (Singapore dollar) to be converted into colombian pesos");
+                    sgd = double.Parse(Console.ReadLine());
+
+                    conversion = sgd * cop;
+
+                    if(!Error)
+                    {
+                        Error = true;
+                        if(sgd < 0)
+                        {
+                            Console.WriteLine("Income a negative value");
+                            Error = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Singapore dollar: {sgd} ---> Colombian pesos {conversion}");
+                            Error = true;
+                        }
+                    }
+                }
+                catch (OverflowException e)
+                {
+                    Console.WriteLine("You enter a very long money");
+                    Error = false;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Did not enter a numeric value");
+                    Error = false;
+                }
+            }
+        }
+
+        //EURO TO COLOMBIAN PESOS
+        public void CurrencyConnverter4()
+        {
+            const double cop = 4461;
+            double eur;
+            double conversion;
+
+            Error = false;
+            while (!Error)
+            {
+                try
+                {
+                    Console.WriteLine("Enter the money (Euro) to be converted into colombian pesos");
+                    eur = double.Parse(Console.ReadLine());
+
+                    conversion = eur * cop;
+
+                    if(!Error)
+                    {
+                        Error = true;
+                        if(eur < 0)
+                        {
+                            Console.WriteLine("Income a negative value");
+                            Error = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Euro: {eur} ---> Colombian pesos {conversion}");
+                            Error = true;
+                        }
+                    }
+                }
+                catch (OverflowException e)
+                {
+                    Console.WriteLine("You enter a very long money");
+                    Error = false;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Did not enter a numeric value");
+                    Error = false;
+                }
+            }
+        }
+
+        //PANAMANIAN BALBOA TO COLOMBIAN PESOS
+        public void CurrencyConnverter5()
+        {
+            const double cop = 4406;
+            double pab_b;
+            double conversion;
+
+            Error = false;
+            while (!Error)
+            {
+                try
+                {
+                    Console.WriteLine("Enter the money (Panamanian balboa) to be converted into colombian pesos");
+                    pab_b = double.Parse(Console.ReadLine());
+
+                    conversion = pab_b * cop;
+
+                    if(!Error)
+                    {
+                        Error = true;
+                        if(pab_b < 0)
+                        {
+                            Console.WriteLine("Income a negative value");
+                            Error = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Panamanian balboa: {pab_b} ---> Colombian pesos {conversion}");
+                            Error = true;
+                        }
+                    }
+                }
+                catch (OverflowException e)
+                {
+                    Console.WriteLine("You enter a very long money");
+                    Error = false;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Did not enter a numeric value");
+                    Error = false;
+                }
+            }
+        }
+
+        public void AreaRectangle()
+        {
+            double height;
+            double base_;
+            double totalAreaRectangle;
+
+            Error = false;
+            while (!Error)
+            {
+                try
+                {
+                    Console.WriteLine("Enter the numbre for the height of the rectangle");
+                    height = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter the numbre for the base of the rectangle");
+                    base_ = double.Parse(Console.ReadLine());
+
+                    totalAreaRectangle = (base_ * height);
+
+                    if(!Error)
+                    {
+                        Error = true;
+                        if(height < 0 || base_ < 0)
+                        {
+                            Console.WriteLine("Income a negative value");
+                            Error = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"The total area of the rectangle is: {totalAreaRectangle}");
+                            Error = true;
+                        }
+                    }
+                }
+                catch (OverflowException e)
+                {
+                    Console.WriteLine("You enter a very long value");
+                    Error = false;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Did not enter a numeric value");
+                    Error = false;
+                }
+            }
+        }
+
+        public void AgeEntered()
+        {
+            int age;
+
+            Error = false;
+            while (!Error)
+            {
+                try
+                {
+                    Console.WriteLine("Enter your age");
+                    age = int.Parse(Console.ReadLine());
+
+                    if (!Error)
+                    {
+                        Error = true;
+                        if (age < 0)
+                        {
+                            Console.WriteLine("Income a negative value");
+                            Error = false;
+                        }
+                        else
+                        {
+                            if (age == 0)
+                            {
+                                Console.WriteLine("Error");
+                            }
+                            else
+                            {
+                                if (age <= 17)
+                                {
+                                    Console.WriteLine("You are a minor");
+                                }
+                                else
+                                {
+                                    if (age >= 18 && age <= 60)
+                                    {
+                                        Console.WriteLine("You are an adult");
+                                    }
+                                    else
+                                    {
+                                        if (age >= 60 && age <= 130)
+                                        {
+                                            Console.WriteLine("You are an older adult");
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("You entered a false age");
+                                        }
+                                    }
+                                }
+                            }
+                            Error = true;
+                        }
+                    }
+                }
+                catch (OverflowException e)
+                {
+                    Console.WriteLine("You enter a very long age");
+                    Error = false;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Did not enter a numeric value");
+                    Error = false;
+                }
+            }
+        }
+
+        public void MultiplicationTable(int x)
+        {
+            int i;
+
+            Error = false;
+            while (!Error)
+            {
+                try
+                {
+                    Console.WriteLine("Enter the number for the multiplication table");
+                    x = int.Parse(Console.ReadLine());
+
+                    if (!Error)
+                    {
+                        Error = true;
+                        if (x < 0)
+                        {
+                            Console.WriteLine("Income a negative value");
+                            Error = false;
+                        }
+                        else
+                        {
+                            for (i = 1; i <= 12; i++)
+                            {
+                                Console.WriteLine($"{x} * {i} = {x * i}");
+                            }
+                            Error = true;
+                        }
+                    }
+                }
+                catch (OverflowException e)
+                {
+                    Console.WriteLine("You enter a very long value");
+                    Error = false;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Did not enter a numeric value");
+                    Error = false;
+                }
+            }
+        }
+    }
+}
+// por que cuando pongo estatico en el metodo no me deja llamarlo en el main
+// cuando hay que usar getter y setter
+// es buena practica dejar metodos vacios
+// por que en el main principal me obliga a inicializar la variable opciones
+// como creo excepciones de letras y no numeros
