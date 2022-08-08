89d836b170b45d41e02797a795d79606dad27395
JHONSEBASTIANRODRIGUEZBOHADA <104032255+JHONSEBASTIANRODRIGUEZBOHADA@users.noreply.github.com>
2022-08-08 09:26:31 -0500

    Add files via upload

---
+++ ConversorNumeroRomano/Informacion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorNumeroRomano
+{
+    internal class Informacion
+    {
+        int option;
+        int n;
+        int u;
+        int d;
+        int c;
+
+        public Informacion() { }
+
+        public int Option { get => option; set => option = value; }
+        public int N { get => n; set => n = value; }
+        public int U { get => u; set => u = value; }
+        public int D { get => d; set => d = value; }
+        public int C { get => c; set => c = value; }
+    }
+}
---
+++ ConversorNumeroRomano/PrincipalMain.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorNumeroRomano
+{
+    internal class PrincipalMain : Informacion
+    {
+        public static void Main(string[] args)
+        {
+            Informacion datos = new Informacion();
+            do
+            {
+                try
+                {
+                    
+
+
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            } while (datos.Option == 0);
+        }
+    }
+}
+
+/*
+UNIDADES
+1 -> I
+2 -> II
+3 -> III
+4 -> IV
+5 -> V
+6 -> VI
+7 -> VII
+8 -> VIIII
+9 -> IX
+10 -> X
+
+DECENAS
+10 -> X
+20 -> XX
+30 -> XXX
+40 -> XL
+50 -> L
+60 -> LX
+70 -> LXX
+80 -> LXXX
+90 -> XC
+
+CENTENAS
+ -> C
+ -> CC
+ -> CCC
+ -> CD
+ -> D
+ -> DC
+ -> DCC
+ -> DCCC
+ -> CM
+*/
